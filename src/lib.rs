50963b8db48e7685ba3383bb643a5d71e6cd02cc
Joris Mertz <info@jorismertz.nl>
2024-06-01 14:41:51 +0200

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,29 @@ pub struct CreateMonitor {
     pub port: Option<i64>,
     pub interval: i64,
 }
+
 #[derive(Debug, Deserialize, Serialize)]
 pub struct CreateMonitorPing {
     pub monitor_id: i64,
     pub timestamp: String,
     pub status: String,
+}
+
+pub enum DatabaseConnection {
+    Rocket(Option<()>),
+    Sqlx(sqlx::Pool<sqlx::Sqlite>),
+}
+
+#[macro_export]
+macro_rules! db_conn {
+    ( $a:ident ) => {{
+        let conn = $a;
+        match conn {
+            DatabaseConnection::Rocket(_) => {
+                let pool = crate::database::initialize().await;
+                DatabaseConnection::Sqlx(pool)
+            }
+            DatabaseConnection::Sqlx(pool) => DatabaseConnection::Sqlx(pool),
+        }
+    }};
 }
